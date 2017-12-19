eae394f921ab0bb19a9a6069f00076ae12d6a707
shynome <shynome@gmail.com>
2017-12-19 20:58:17 +0800

    report

---
+++ src/global.d.ts
@@ -0,0 +1,5 @@
+import { VueConstructor } from "vue";
+declare global {
+  type Vue = VueConstructor
+  const Vue: VueConstructor
+}
---
+++ src/test2.js
@@ -0,0 +1,8 @@
+Vue.component('x',{
+  props:{
+    x:{ type:String }
+  },
+  created(){
+    this.x = 7
+  }
+})
